80c93e61e7d9636fe664d32bdee509264ffb2b18
Adam Tang <adamtang2004@hotmail.com>
2026-04-06 00:57:46 +0100

    ghostty colourscheme changed to hkr, neogen plugin installed for docstrings, and lazy config changed

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -27,7 +27,6 @@ require("lazy").setup({
     -- import your plugins
     { import = "plugins" },
     { import = "plugins.lsp" },
-    { import = "plugins.vim_motions" },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
--- nvim/.config/nvim/lua/plugins/neogen.lua
+++ nvim/.config/nvim/lua/plugins/neogen.lua
@@ -1,6 +1,33 @@
 return {
     "danymat/neogen",
-    config = true,
-    -- Uncomment next line if you want to follow only stable versions
-    -- version = "*" 
+    config = function()
+        require('neogen').setup ({
+            enabled = true,             --if you want to disable Neogen
+            input_after_comment = true, -- (default: true) automatic jump (with insert mode) on inserted annotation
+            -- jump_map = "<C-e>"       -- (DROPPED SUPPORT, see [here](#cycle-between-annotations) !) The keymap in order to jump in the annotation fields (in insert mode)
+            languages = {
+                python = {
+                    template = {
+                        annotation_convention = "reST",
+                    },
+                },
+            },
+        })
+
+        vim.keymap.set("n", "<leader>nf", function()
+            require("neogen").generate({ type = "func" })
+        end, { desc = "Docstring (function)" })
+
+        vim.keymap.set("n", "<leader>nc", function()
+            require("neogen").generate({ type = "class" })
+        end, { desc = "Docstring (class)" })
+
+        vim.keymap.set("n", "<leader>nt", function()
+            require("neogen").generate({ type = "type" })
+        end, { desc = "Docstring (type)" })
+
+        vim.keymap.set("n", "<leader>nn", function()
+            require("neogen").generate({ type = "type" })
+        end, { desc = "Docstring (type)" })
+    end,
 }
